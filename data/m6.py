97001ad83f8bf6254daa12abfef2b74674035300
NoSegfault <jerrynjzhang@gmail.com>
2017-08-20 10:51:21 -0500

    modify m6

--- data/m6.py
+++ data/m6.py
@@ -14,28 +14,28 @@ def main():
 
     content = [x.strip() for x in content]
     v_to = {}
+    for i in range(n):
+        v_to[i] = []
     for line in content[5:]:
         pair = line.split()
-        if int(pair[0]) not in v_to:
-            v_to[int(pair[0])]=[]
         v_to[int(pair[0])].append(int(pair[1]))
 
     l = []
     mark = []
     for item in v_to:
-        # print(item, v_to[item])
+        #print(item, v_to[item])
         els = [list(x) for x in itertools.combinations(v_to[item], 2)]
         for it in els:
-            if it[1] in v_to and it[1] in v_to:
-                if it[0] in v_to[it[1]] and it[1] in v_to[it[0]]:
-                    mat = np.zeros(n)
-                    mat[item] = 1
-                    mat[it[0]] = 1
-                    mat[it[1]] = 1
-                    l.append(mat)
+            #print item,it[0],it[1]
+            if it[0] in v_to[it[1]] and it[1] in v_to[it[0]] and item not in v_to[it[0]] and item not in v_to[it[1]]:
+                mat = np.zeros(n)
+                mat[item] = 1
+                mat[it[0]] = 1
+                mat[it[1]] = 1
+                l.append(mat)
 
-                    sub = [item, it[0], it[1]]
-                    mark.append(sub)
+                sub = [item, it[0], it[1]]
+                mark.append(sub)
 
     #print len(l)
     #print mark
@@ -52,11 +52,15 @@ def main():
             if find == 1:
                 break
         if find == 0:
-            # need a new set
+            #need a new set
+            #print("need a new set")
+            #print(row)
             d = set()
             d.update(row)
             set_list.append(d)
 
+    #print('set_list before merge is: ')
+    #print(set_list)
 
     for i in range(0, len(set_list)):
         for j in range(i+1, len(set_list)):
@@ -70,7 +74,7 @@ def main():
     print('size of lcc is {}'.format(new_n))
     lcc = []
     lcc.append(list(set_list[0]))
-    print lcc
+    #print lcc
     for i in range(len(l)):
         if mark[i][0] in set_list[0]:
             #print mark[i]
@@ -79,7 +83,8 @@ def main():
     #print lcc
     print('number of hyperedges is {}'.format(len(lcc)-1))
 
-    with open('m6.csv','w') as f:
+
+    with open('Florida-m6.csv','w') as f:
         writer = csv.writer(f)
         writer.writerows(lcc);
         f.close()
